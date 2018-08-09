8454ddb78ddc6b57f5d0b295024975e0ebdc9170
Praduman <praduman1417@gmail.com>
2018-08-09 17:17:23 +0530

    added the keys

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key ="${var.secret_key}"
+    region = "${var.region}"
+}
+
+resource "aws_instance" "instance" {
+    ami = "ami-b70554c8"
+    instance_type = "t2.micro"
+    
+  
+}
+
+
+resource "aws_subnet" "public_subnet" {
+     
+
+}
+
+
+resource "aws_security_group" "public_security_group" {
+  
+}
+
+
 
 
 
--- variables.tf
+++ variables.tf
@@ -1,1 +1,5 @@
-variable "region" {}
+variable "region" {
+    default = "us-east-1"
+}
+variable "access_key" {}
+variable "secret_key" {}
