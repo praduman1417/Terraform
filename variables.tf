5db6ba09c57106bec36125afc57e8a6b9d9a7157
Praduman <praduman1417@gmail.com>
2019-03-19 10:35:42 +0530

    commenting the variables

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "region" {
     default = "ap-southeast-1"
 }
-variable "access_key" {}
-variable "secret_key" {}
+#variable "access_key" {}
+#variable "secret_key" {}
 
 #### instance details ####
 
